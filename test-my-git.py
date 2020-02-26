d38fa93a6a93b0435e92dfb88204a9586dd067c1
sai <saikirankondapalli03@gmail.com>
2020-02-26 02:23:28 -0500

    Mock get methods to  remove  dependency on  git api  for unit  tests

--- test-my-git.py
+++ test-my-git.py
@@ -1,32 +1,34 @@
 import unittest
 import requests
 from my_git import get_user_git_data
+from unittest.mock import patch
+
 
 class TestMyGitData(unittest.TestCase):
 
+    
     def test_get_user_git_data(self):
-        self.assertIsNone(get_user_git_data(''))
-
-    def test_get_user_git_data_invalid_data(self):
-        self.assertIsNotNone(get_user_git_data('12'))
-
+        with patch('requests.get') as mock_request:
+                mock_request.return_value.status_code = 403
+                result=get_user_git_data('')
+        self.assertEquals(result,'Forbidden')
+    
+    
     def test_get_user_git_data_my_data(self):
-        result=get_user_git_data('saikirankondapalli03')
-        self.assertEqual(result
-            , [
-                'Repository: Algo_Ds_Notes has 30 number of commits',
-                'Repository: Angular2SampleProj has 1 number of commits',
-                'Repository: AngularJs has 5 number of commits',
-                'Repository: gedcom_Agile has 30 number of commits',
-                'Repository: HW09 has 2 number of commits',
-                'Repository: Node-Angular2Example has 3 number of commits',
-                'Repository: PlanPackRepeat has 30 number of commits',
-                'Repository: planpackrepeat-backend has 30 number of commits',
-                'Repository: pythonmath has 9 number of commits',
-                'Repository: spring-boot has 30 number of commits',
-                'Repository: spring-boot-quartz-demo has 6 number of commits',
-                'Repository: SSW-567 has 10 number of commits'
-            ])
+        with patch('requests.get') as mock_request:
+                mock_request.return_value.status_code = 201
+                result=get_user_git_data('')
+        self.assertIsNone(result)
+    
+    
+    def test_get_user_git_data_my_data_1(self):
+        with patch('requests.get') as mock_request:
+                mock_request.return_value.status_code = 200
+                mock_request.return_value.json.return_value  = [{"name":"Algo_Ds_Notes"},{"name":"Angular2SampleProj"}]
+                result=get_user_git_data('')
+        self.assertEqual(result,[ 'Repository: Algo_Ds_Notes has 2 number of commits','Repository: Angular2SampleProj has 2 number of commits'])
+               
+    
 
 
 if __name__ == '__main__':
