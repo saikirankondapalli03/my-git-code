ba957e251b8bb8e7ae6056728c74a4380fedceca
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2020-03-07 19:09:07 -0500

    Update test-my-git.py

--- test-my-git.py
+++ test-my-git.py
@@ -13,20 +13,21 @@ class TestMyGitData(unittest.TestCase):
     def test_get_user_git_data_my_data(self):
         result=get_user_git_data('saikirankondapalli03')
         self.assertEqual(result
-            , [
-                'Repository: Algo_Ds_Notes has 30 number of commits',
-                'Repository: Angular2SampleProj has 1 number of commits',
-                'Repository: AngularJs has 5 number of commits',
-                'Repository: gedcom_Agile has 30 number of commits',
-                'Repository: HW09 has 2 number of commits',
-                'Repository: Node-Angular2Example has 3 number of commits',
-                'Repository: PlanPackRepeat has 30 number of commits',
-                'Repository: planpackrepeat-backend has 30 number of commits',
-                'Repository: pythonmath has 9 number of commits',
-                'Repository: spring-boot has 30 number of commits',
-                'Repository: spring-boot-quartz-demo has 6 number of commits',
-                'Repository: SSW-567 has 10 number of commits'
-            ])
+            , ['Repository: Algo_Ds_Notes has 30 number of commits', 
+               'Repository: Angular2SampleProj has 1 number of commits', 
+               'Repository: AngularJs has 5 number of commits', 
+               'Repository: gedcom_Agile has 30 number of commits', 
+               'Repository: HW-05-01 has 2 number of commits', 
+               'Repository: HW09 has 2 number of commits', 
+               'Repository: my-git-code has 8 number of commits', 
+               'Repository: Node-Angular2Example has 3 number of commits', 
+               'Repository: PlanPackRepeat has 30 number of commits', 
+               'Repository: planpackrepeat-backend has 30 number of commits', 
+               'Repository: pythonmath has 9 number of commits', 
+               'Repository: reinforcement-learning has 30 number of commits', 
+               'Repository: spring-boot has 30 number of commits', 
+               'Repository: spring-boot-quartz-demo has 6 number of commits', 
+               'Repository: SSW-567 has 10 number of commits'])
 
 
 if __name__ == '__main__':
